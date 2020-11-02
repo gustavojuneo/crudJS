df7e203c2074880f934e6b0041af9c35f444c60b
Gustavo Juneo Santos <gustavojuneo@outlook.com>
2020-11-02 19:59:10 -0300

    Created server and configurations

---
+++ admin.js
@@ -0,0 +1,16 @@
+const AdminBro = require('admin-bro')
+const AdminBroExpress = require('@admin-bro/express')
+
+const adminBro = new AdminBro({
+  databases: [],
+  rootPath: '/admin'
+})
+
+const router = AdminBroExpress.buildRouter(adminBro)
+
+const express = require('express')
+const server = express()
+
+server
+  .use(adminBro.options.rootPath, router)
+  .listen(5500, () => console.log('Server started'))
